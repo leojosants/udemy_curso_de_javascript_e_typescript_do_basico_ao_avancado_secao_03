121e46eb615a9cd11fa3ce0fa278609308cb5299
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-24 21:02:23 -0300

    class 38 complement

--- 38_operadores_logicos/app.js
+++ 38_operadores_logicos/app.js
@@ -39,7 +39,199 @@ function startApp() {
         const password = (Math.floor(Math.random() * 5)).toString();
         const logged = (user === 'Leonardo') && (password === '0');
         console.log('Usuário logado: ', logged);
+        example04();
     };
-    example04();
+
+    function comparisonAND() {
+        /* true and true */
+        const first_situation = {
+            test_01: true,
+            test_02: true,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`\n${this.test_01}  && ${this.test_02}  = ${result}`);
+            },
+        };
+
+        /* true and false */
+        const second_situation = {
+            test_01: true,
+            test_02: false,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`${this.test_01}  && ${this.test_02} = ${result}`);
+            },
+        };
+
+        /* false and false */
+        const third_situation = {
+            test_01: false,
+            test_02: false,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`${this.test_01} && ${this.test_02} = ${result}`);
+            },
+        };
+
+        /* false and true */
+        const fourth_situation = {
+            test_01: false,
+            test_02: true,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`${this.test_01} && ${this.test_02}  = ${result}`);
+            },
+        };
+
+        first_situation.status();
+        second_situation.status();
+        third_situation.status();
+        fourth_situation.status();
+    };
+    comparisonAND();
+
+    function comparisonOR() {
+        /* true and true */
+        const first_situation = {
+            test_01: true,
+            test_02: true,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`\n${this.test_01}  || ${this.test_02}  = ${result}`);
+            },
+        };
+
+        /* true and false */
+        const second_situation = {
+            test_01: true,
+            test_02: false,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`${this.test_01}  || ${this.test_02} = ${result}`);
+            },
+        };
+
+        /* false and false */
+        const third_situation = {
+            test_01: false,
+            test_02: false,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`${this.test_01} || ${this.test_02} = ${result}`);
+            },
+        };
+
+        /* false and true */
+        const fourth_situation = {
+            test_01: false,
+            test_02: true,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`${this.test_01} || ${this.test_02}  = ${result}`);
+            },
+        };
+
+        first_situation.status();
+        second_situation.status();
+        third_situation.status();
+        fourth_situation.status();
+    };  
+    comparisonOR();
+
+    function comparisonAND_NOT() {
+        /* true and true */
+        const first_situation = {
+            test_01: true,
+            test_02: true,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`\n${this.test_01}  && ${this.test_02}  = !${result}  --> ${!result}`);
+            },
+        };
+
+        /* true and false */
+        const second_situation = {
+            test_01: true,
+            test_02: false,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`${this.test_01}  && ${this.test_02} = !${result} --> ${!result}`);
+            },
+        };
+
+        /* false and false */
+        const third_situation = {
+            test_01: false,
+            test_02: false,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`${this.test_01} && ${this.test_02} = !${result} --> ${!result}`);
+            },
+        };
+
+        /* false and true */
+        const fourth_situation = {
+            test_01: false,
+            test_02: true,
+            status() {
+                const result = this.test_01 && this.test_02;
+                console.log(`${this.test_01} && ${this.test_02}  = !${result} --> ${!result}`);
+            },
+        };
+
+        first_situation.status();
+        second_situation.status();
+        third_situation.status();
+        fourth_situation.status();
+    };
+    comparisonAND_NOT();
+
+    function comparisonOR_NOT() {
+        /* true and true */
+        const first_situation = {
+            test_01: true,
+            test_02: true,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`\n${this.test_01}  || ${this.test_02}  = !${result}  --> ${!result}`);
+            },
+        };
+
+        /* true and false */
+        const second_situation = {
+            test_01: true,
+            test_02: false,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`${this.test_01}  || ${this.test_02} = !${result}  --> ${!result}`);
+            },
+        };
+
+        /* false and false */
+        const third_situation = {
+            test_01: false,
+            test_02: false,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`${this.test_01} || ${this.test_02} = !${result} --> ${!result}`);
+            },
+        };
+
+        /* false and true */
+        const fourth_situation = {
+            test_01: false,
+            test_02: true,
+            status() {
+                const result = this.test_01 || this.test_02;
+                console.log(`${this.test_01} || ${this.test_02}  = !${result}  --> ${!result}`);
+            },
+        };
+
+        first_situation.status();
+        second_situation.status();
+        third_situation.status();
+        fourth_situation.status();
+    };  
+    comparisonOR_NOT();
 };
 startApp();
